536beac07f2c7a667f12f6d46cbda905cb605e44
13wjdgk <13wjdgk@naver.com>
2024-07-03 16:35:14 +0900

    feat : 외부 브로커 연결

--- src/main/java/Arambyeol/chat/global/socket/WebSocketConfig.java
+++ src/main/java/Arambyeol/chat/global/socket/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package Arambyeol.chat.global.socket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -14,12 +15,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
 	private final JwtChannelInterceptor jwtChannelInterceptor;
+	@Value("${spring.activemq.user}")
+	private String user;
+	@Value("${spring.activemq.password}")
+	private String password;
+	@Value("${spring.activemq.host}")
+	private String host;
+	@Value("${spring.activemq.port}")
+	private int port;
+
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry){
-		//해당 prefix를 구독하는 클라이언트에세 메세지를 보낸다.
-		registry.enableSimpleBroker("/sub");
+		//해당 prefix를 구독하는 클라이언트에게 메세지를 보낸다.
+		registry.enableStompBrokerRelay("/sub")
+			.setRelayHost(host).setRelayPort(port)
+			.setSystemLogin(user).setSystemPasscode(password)
+			.setClientLogin(user).setClientPasscode(password);
 		// 메세지 발행 요청 prefix, /pub으로 시작하는 메세지만 해당 Broker에서 받아와서 처리한다.
 		registry.setApplicationDestinationPrefixes("/pub");
 	}
